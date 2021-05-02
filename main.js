d041232e730d464916da00f1d50e03ea76e51bdc
Amandeep Singh <asinghsanity@gmail.com>
2021-05-02 12:46:26 -0400

    Created the function int_to_word. The function turns int values into their word form.

--- main.js
+++ main.js
@@ -1,12 +1,53 @@
 var args = process.argv.slice(2); // Turns args into a list (First 2 args are system related)
 //console.log(args);
 
+var final = []
 for (var i = 0; i < args.length; i++){
     console.log(args[i]);
-    var str = args[i];
+    var element = args[i];
+    var string = "";
 
-    for (var j = 0; j < str.length; j++) {
-        console.log(str[j])
+    for (var j = 0; j < element.length; j++) {
+        //console.log(element[j]);
+        var x = int_to_word(element[j]);
+        string = string.concat(x);
     }
+    console.log(string);
+}
 
+function int_to_word(character){
+    var str = "";
+    switch(character){
+        case "1":
+            str = "One";
+            break;
+        case "2":
+            str = "Two";
+            break;
+        case "3":
+            str = "Three";
+            break;
+        case "4":
+            str = "Four";
+            break;
+        case "5":
+            str = "Five";
+            break;
+        case "6":
+            str = "Six";
+            break;
+        case "7":
+            str = "Seven";
+            break;
+        case "8":
+            str = "Eight";
+            break;
+        case "9":
+            str = "Nine";
+            break;
+        case "0":
+            str = "Zero";
+            break;
+    }
+    return str;
 }
