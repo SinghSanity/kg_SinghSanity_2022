aacaee400e231ae5a064fe2c8fc122a3d7febd4f
Amandeep Singh <asinghsanity@gmail.com>
2021-05-02 12:30:06 -0400

    Added two loops. For the args, and for the chars.

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 var args = process.argv.slice(2); // Turns args into a list (First 2 args are system related)
-console.log(args);
+//console.log(args);
+
+for (var i = 0; i < args.length; i++){
+    console.log(args[i]);
+    var str = args[i];
+
+    for (var j = 0; j < str.length; j++) {
+        console.log(str[j])
+    }
+
+}
