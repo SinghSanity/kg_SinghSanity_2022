e3b24dc0e9a69ffaba0e9d2139b26e2db51ebf21
Amandeep Singh <asinghsanity@gmail.com>
2021-05-02 16:26:34 -0400

    Added edge case checks: No-input & bad input. No Input implies 0 args given, Bad Input implies non-ints given. Also moved around some functions & cleaned README.

--- main.js
+++ main.js
@@ -1,22 +1,5 @@
-var args = process.argv.slice(2); // Turns args into a list (First 2 args are system related)
-//console.log(args);
-
-var final = [];
-for (var i = 0; i < args.length; i++){
-    //console.log(args[i]);
-    var element = args[i];
-    var string = "";
-
-    for (var j = 0; j < element.length; j++) {
-        //console.log(element[j]);
-        var x = int_to_word(element[j]);
-        string = string.concat(x);
-    }
-    //console.log(string);
-    final.push(string);
-}
-
 function print_format(lst){
+    // 'print_format' outputs the elements in the same way shown in the prompt.
     var format = "";
     for (var i =0; i<lst.length; i++){
         if (i == lst.length-1){
@@ -30,9 +13,8 @@ function print_format(lst){
     console.log(format);
 }
 
-print_format(final);
-
 function int_to_word(character){
+    // int_to_word changes the integer inputted to its phonetic equivalent.
     var str = "";
     switch(character){
         case "1":
@@ -70,4 +52,55 @@ function int_to_word(character){
             break;
     }
     return str;
+}
+
+function bad_input(character){
+    // bad_input is used to check if a character is an integer.
+    const string = '1234567890';
+    if (!string.includes(character)) {
+        return true;
+    }
+}
+
+// Turns args into a list (First 2 args are system related)
+var args = process.argv.slice(2); 
+//console.log(args.length);
+
+// First check to see if we have any arguments provided. If not, we send an Error.
+if (args.length === 0){
+    console.log("Error: You did not enter any arguments.")
+}
+
+else{
+    var final = [];
+    var good_input = true;
+
+    for (var i = 0; i < args.length; i++){
+        //console.log(args[i]);
+        var element = args[i];
+        var string = "";
+        if (!good_input){
+            break;
+        }
+        for (var j = 0; j < element.length; j++) {
+
+            if (bad_input(element[j])){
+                good_input = false;
+                break;
+            }
+
+            //console.log(element[j]);
+            var x = int_to_word(element[j]);
+            string = string.concat(x);
+        }
+        //console.log(string);
+        final.push(string);
+    }
+
+    if (good_input){
+        print_format(final);
+    }
+    else{
+        console.log("Error: You entered a non-integer argument");
+    }
 }
